3098c4e970091e28de85e9aea1437ac7a88ddf02
Kristian Coulson <kristiancoulson@gmail.com>
2024-03-06 19:10:46 +0000

    remove purge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import purgecss from "astro-purgecss";
+// import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), purgecss()]
+  integrations: [tailwind()],
 });

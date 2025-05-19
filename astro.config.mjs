98bbba6ef787ea5f672355b67e3e1e7f417cafe0
Robert Coulson <43724396+Coulsyltd@users.noreply.github.com>
2025-05-19 15:05:18 +0100

    domain added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-domain.com', // <-- Replace with your actual site URL
+  site: 'https://coulsyfiredoors.co.uk', // <-- Replace with your actual site URL
   integrations: [
     tailwind(),
     sitemap(),
